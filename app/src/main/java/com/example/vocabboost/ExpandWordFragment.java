81b8841a99d37879b47fe9896512f4b00158f9d1
Akash <thatsakashpal@gmail.com>
2022-07-19 19:01:47 +0530

    Pronounciation of each word is working perfectly

--- app/src/main/java/com/example/vocabboost/ExpandWordFragment.java
+++ app/src/main/java/com/example/vocabboost/ExpandWordFragment.java
@@ -6,9 +6,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.app.Fragment;
+
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +18,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -82,10 +90,15 @@ public class ExpandWordFragment extends Fragment implements View.OnClickListener
         return(layout);
     }
     @Override
-    public void onClick(View v) {
+    public void onClick(View v) {Log.d("17th","Reached");
         if(v.getId()==R.id.audio) {
             String url="https://ssl.gstatic.com/dictionary/static/sounds/20200429/hefty--_gb_2.mp3";
-            playSound(url);return;
+            Log.d("17th","Reached");
+            new doIT().execute();
+            Log.d("17th","Reached");
+            //url=findUrl();
+            //playSound(url);
+            return;
         }
         Log.d("!!Second:","Click0");
         WordInsertionFromWordInsertion fragment=new WordInsertionFromWordInsertion();
@@ -103,13 +116,64 @@ public class ExpandWordFragment extends Fragment implements View.OnClickListener
         mp=new MediaPlayer();
         mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
-            Log.d("15th","Success");
+            Log.d("15th",url);
             mp.setDataSource(url);
             mp.prepare();
             mp.start();
             Log.d("15th","Success");
         } catch (IOException e) {
+            Log.d("15th","Song Failed");
             e.printStackTrace();
+            //Toast.makeText(context,"Either "+w+" is not a valid word or your internet connection is not stable",Toast.LENGTH_SHORT).show();
+        }
+    }
+    private String findUrl()
+    {
+        try{
+        String url="https://www.google.com/search?q=hefty+meaning";
+            Log.d("17th","1st");
+        Document doc= Jsoup.connect(url).get();
+            Log.d("17th","2nd");
+            Elements e=doc.select("div.brWULd");
+            int size=e.size();
+            String p="";
+            Log.d("17th",p);
+            for(int i=0;i<size;i++)
+                p = e.select("div.brWULd").select("source").eq(i).attr("src");
+            Log.d("17th",p);
+        }
+        catch (Exception e){Log.d("17th","Fail");
+            Log.e("MYAPP", "exception", e);
+        }
+        return("Akash");
+    }
+    public class doIT extends AsyncTask<Void,Void,Void> {
+        String url,p;
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String url="https://www.google.com/search?q="+w+"+meaning";
+                Log.d("17th","1st");
+                Document doc= Jsoup.connect(url).get();
+                Log.d("17th","1st");
+                Elements e=doc.select("div.brWULd");
+                int size=e.size();
+                String p="";
+                Log.d("17th",p);
+                for(int i=0;i<size;i++)
+                    p = e.select("div.brWULd").select("source").eq(i).attr("src");
+                Log.d("17th",p);
+                playSound("https:"+p);
+            } catch (IOException e) {
+                Log.d("17th","Fail");
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
         }
     }
 }
